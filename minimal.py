7965aeb64645940453a30c064babd3f6c9b209da
AbeTavarez <abeincwireless@gmail.com>
2021-05-02 19:32:40 -0400

    blog page

--- minimal.py
+++ minimal.py
@@ -2,8 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+# Home Page
 @app.route('/')
 def main():
-    return 'Server Online...'
+    return 'HOME PAGE'
 
+
+# Blog
+@app.route('/blog')
+def blog_page():
+    return 'BLOG PAGE'
